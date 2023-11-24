6d32a301502163929180a020086ead134497617a
Victor Aremu <victor.olorunbunmi@gmail.com>
2023-11-24 03:20:56 +0100

    feat: intial commit

---
+++ libs/app-icon/src/lib.rs
@@ -0,0 +1,19 @@
+use std::path::Path;
+use thiserror::Error;
+
+#[cfg(target_os = "macos")]
+mod macos;
+
+#[derive(Error, Debug)]
+#[error("get app icon error")]
+pub struct GetAppIconError {
+    #[cfg(target_os = "macos")]
+    #[from]
+    source: macos::request::GetIconError,
+}
+
+#[cfg(target_os = "macos")]
+pub fn get_app_icon(app_path: &Path, save_path: &Path, size: f64) -> Result<(), GetAppIconError> {
+    macos::request::get_icon(app_path, save_path, size)?;
+    Ok(())
+}
---
+++ shared/rollup.config.mjs
@@ -0,0 +1,46 @@
+import { builtinModules } from "module";
+
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+
+/**
+ * Create a base rollup config
+ * @param {Record<string,any>} pkg Imported package.json
+ * @param {string[]} external Imported package.json
+ * @returns {import('rollup').RollupOptions}
+ */
+export function createConfig({ input = "index.ts", pkg, external = [] }) {
+	return [
+		{
+			input,
+			external: Object.keys(pkg.dependencies || {})
+				.concat(Object.keys(pkg.peerDependencies || {}))
+				.concat(builtinModules)
+				.concat(external),
+			onwarn: (warning) => {
+				throw Object.assign(new Error(), warning);
+			},
+			strictDeprecations: true,
+			output: {
+				file: pkg.module,
+				format: "es",
+				sourcemap: true,
+			},
+			plugins: [typescript({ sourceMap: true })],
+		},
+		{
+			input,
+			onwarn: (warning) => {
+				throw Object.assign(new Error(), warning);
+			},
+			strictDeprecations: true,
+			output: {
+				file: pkg.browser,
+				format: "es",
+				sourcemap: true,
+				entryFileNames: "[name].min.js",
+			},
+			plugins: [resolve(), typescript({ sourceMap: true })],
+		},
+	];
+}
