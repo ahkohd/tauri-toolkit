f4ff1101a556c316f64306cf0994605e871d2620
Lilit0x <baasisek01@gmail.com>
2024-05-29 15:02:32 +0100

    test: tested image saving

--- libs/app-icon/src/windows/tests.rs
+++ libs/app-icon/src/windows/tests.rs
@@ -24,6 +24,16 @@ fn save_path_parent_does_not_exist() {
 }
 
 #[test]
+fn image_save_failure() {
+    let app_path = Path::new(r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe");
+    // eleveted access required to write to this folder
+    let save_path = Path::new(r"C:\Windows\System32\forbidden_icon.png"); 
+    let result = get_icon(app_path, save_path, 32.0);
+    assert!(result.is_err());
+    assert_eq!(result.unwrap_err(), GetIconError::ImageSaveError);
+}
+
+#[test]
 fn it_works() {
     let app_path = Path::new(r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe");
     let save_path = Path::new(r"C:\Windows\Temp\edge.png");
