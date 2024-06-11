4a78ebc1e526b2c901d1471bee2e5eeb5b0009f2
Victor Aremu <me@victorare.mu>
2024-06-11 14:54:04 +0100

    chore: improve tests

--- libs/menubar/src/macos/tests.rs
+++ libs/menubar/src/macos/tests.rs
@@ -1,10 +1,8 @@
 #![cfg(test)]
 
-use crate::macos::menubar;
-
 #[test]
-fn it_get_menubar_for_monitor() {
-    let menubar_height = menubar::get_height();
+fn it_gets_the_menubar_height() {
+    let menubar_height = super::menubar::get_height();
 
     assert!(menubar_height >= 22.0);
 }
