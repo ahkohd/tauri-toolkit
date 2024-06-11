4eaae244c1ae0da13e8e10508340a144127bf876
Victor Aremu <me@victorare.mu>
2024-06-11 14:42:13 +0100

    feat: improve menubar implementation

--- libs/menubar/src/macos/tests.rs
+++ libs/menubar/src/macos/tests.rs
@@ -1,16 +1,10 @@
 #![cfg(test)]
 
-use crate::macos::menubar::get_menubar;
+use crate::macos::menubar;
 
 #[test]
 fn it_get_menubar_for_monitor() {
-    let menubar = get_menubar();
+    let menubar_height = menubar::get_height();
 
-    assert!(menubar.is_some());
-
-    let menubar = menubar.unwrap();
-
-    assert!(menubar.height() > 0.0);
-
-    assert!(menubar.scale_factor() > 0.0);
+    assert!(menubar_height >= 22.0);
 }
