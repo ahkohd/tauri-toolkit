c42d04710267b9c1459bd36f53b3487b6de241e2
Victor Aremu <me@victorare.mu>
2024-06-15 12:30:24 +0100

    refactor: rename test

--- libs/menubar/src/macos/tests.rs
+++ libs/menubar/src/macos/tests.rs
@@ -3,7 +3,7 @@
 use crate::macos::menubar;
 
 #[test]
-fn it_get_menubar_for_monitor() {
+fn it_gets_the_menubar_height() {
     let menubar_height = menubar::get_height();
 
     assert!(menubar_height >= 22.0);
